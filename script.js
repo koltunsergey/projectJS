8ea01b2139e97956727c3519abf0f6ecd4428eba
Sergey Koltun <amhardworker@gmail.com>
2020-08-05 20:09:35 +0300

    sound

--- script.js
+++ script.js
@@ -7,27 +7,15 @@ let imgW = 348; // размер спрайта корабля
 let imgH = 145; // размер спрайта корабля
 let step = 1024 / 8 // размеры одного астероида
 let step2 = 900 / 9 // размеры одного взрыва
-
-function vibrate(val){
-    if("vibrate" in navigator)  return navigator.vibrate(val);
-    if("oVibrate" in navigator)  return navigator.oVibrate(val);
-    if("mozVibrate" in navigator)  return navigator.mozVibrate(val);
-    if("webkitVibrate" in navigator)  return navigator.webkitVibrate(val);
-    document.getElementById('error').innerHTML = "Ваш браузер не поддерживает vibration Api .. попробуйте открыть пример в мобильном fixefox, там все точно работает";
-  }
-  
-  function infiniteVibrate(val, interval) {
-    stopVibrate();
-    vInterval = setInterval(function() {
-       vibrate(val);
-    }, interval);
-  }
-  
-  function stopVibrate() {
-    if(typeof(vInterval)!=="undefined") clearInterval(vInterval);
-    vibrate(0);
-  }
-
+let clickAudio = new Audio("sound.wav");
+// подстраиваемся под ширину окна браузера
+let w = canvas.width = window.innerWidth;
+let h = canvas.height = window.innerHeight;
+// подстраиваемся под ресайз окна
+window.addEventListener("resize", () => {
+    w = canvas.width = window.innerWidth;
+    h = canvas.height = window.innerHeight;
+})
 
 function random(min, max) {
     min = Math.ceil(min);
@@ -35,19 +23,61 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-// подстраиваемся под ширину окна браузера
-let w = canvas.width = window.innerWidth;
-let h = canvas.height = window.innerHeight;
+
 
 let asteroids = [];
 let rockets = [];
 let exploz = [];
 
+// объект игрока
+let player = {
+    shipW: 348, // размер спрайта корабля
+    shipH: 145, // размер спрайта корабля
+    x: w / 2 - 150, // начальные точки корабля
+    y: h - 100,   // начальные точки корабля
+    dx: 0,
+    dy: 0,
+    vx: 10,
+    vy: 10,
+    // move: function () {
+    //     this.x += this.dx;
+    //     this.y += this.dy;
+    // },
+    // stop: function () {
+    //     this.dx = 0;
+    //     this.dy = 0;
+    // },
+    lives: 3,
+    score: 0,
+};
+
+// события touch
+canvas.addEventListener('touchstart', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.targetTouches.length == 1) {
+        fire() /*Ваш код*/
+    }
+}, false);
+canvas.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.targetTouches.length == 1) {
+        var touch = event.targetTouches[0];
+        player.x = touch.pageX;
+        player.y = touch.pageY;
+    }
+}, false);
+canvas.addEventListener('touchend', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    /* Здесь ваш код обработки события*/
+}, false);
+
 // навешиваем листенер события движеня мыши и перемещаем туда игрока
 canvas.addEventListener("mousemove", function (EO) {
     player.x = EO.offsetX - imgW / 4;
     player.y = EO.offsetY - imgH / 4;
-
 });
 
 canvas.addEventListener("click", fire);
@@ -65,26 +95,7 @@ function fire() {
     });
 }
 
-player = {
-    shipW: 348, // размер спрайта корабля
-    shipH: 145, // размер спрайта корабля
-    x: w/2 - 150, // начальные точки корабля
-    y: h - 100,   // начальные точки корабля
-    dx: 0,
-    dy: 0,
-    vx: 10,
-    vy: 10,
-    // move: function () {
-    //     this.x += this.dx;
-    //     this.y += this.dy;
-    // },
-    // stop: function () {
-    //     this.dx = 0;
-    //     this.dy = 0;
-    // },
-    lives: 3,
-    score: 0,
-};
+
 
 // двигаем игрока с помощью клавиатуры
 document.addEventListener('keydown', function (EO) {
@@ -106,15 +117,6 @@ document.addEventListener('keydown', function (EO) {
             break;
     }
 });
-
-// canvas.addEventListener('keyup', function (EO) {
-//     player.stop();
-// });
-
-// if (player.dx || player.dy) {
-//     player.move();
-// };
-
 
 // кросбраузерность для requestAnimationFrame
 let RAF = (function () {
@@ -151,14 +153,27 @@ let sprites = {
     expl: undefined,
 };
 
+
+function clickSoundInit() {
+    clickAudio.play(); // запускаем звук
+    clickAudio.pause(); // и сразу останавливаем
+}
+
+function clickSound() {
+    clickAudio.currentTime = 0; // в секундах
+    clickAudio.play();
+}
+
+for (let key in sprites) {
+    sprites[key] = new Image();
+    sprites[key].src = "" + key + ".png";
+};
+
 // отрисовываем игровое поле
 function render() {
     context.clearRect(0, 0, w, h);
-        // загружаем все спрайты циклом по имени ключа
-    for (let key in sprites) {
-        sprites[key] = new Image();
-        sprites[key].src = "" + key + ".png";
-    };
+    // загружаем все спрайты циклом по имени ключа
+
 
     // рисуем подобие космического неба в нашем канвасе
     let grad = context.createLinearGradient(0, 0, 0, h);
@@ -188,62 +203,21 @@ function render() {
 
     asteroids.forEach(element => {
         // рисуем астероид
-        context.drawImage(sprites.aster, element.zx, element.zy, step, step, element.x, element.y, step/1.5, step/1.5);
+        context.drawImage(sprites.aster, element.zx, element.zy, step, step, element.x, element.y, step / 1.5, step / 1.5);
     });
 
     exploz.forEach(element => {
         // рисуем взрыв
-        context.drawImage(sprites.expl, step2*Math.floor(element.ax), step2*Math.floor(element.ay), step2, step2, element.x, element.y, step2, step2);
+        context.drawImage(sprites.expl, step2 * Math.floor(element.ax), step2 * Math.floor(element.ay), step2, step2, element.x, element.y, step2, step2);
     });
 
     // выводим на экран счёт
     context.fillStyle = "white";
-    context.font='24px sans-serif';
+    context.font = '24px sans-serif';
     context.fillText("SCORE: " + player.score, 50, 50, 200);
     context.fillText("LIVES: " + player.lives, 50, 100, 200);
-    
+
 }
-
-// заставляем звёзды двигаться по игрику вниз и удаляем из массива улетевшие
-// будем использовать эту же функцию и для ракет
-function arrMove(arr) {
-    arr.forEach(function (element, index, object) {
-        element.y = element.y + element.s;
-        if (element.y >= h || element.y <= 0) {
-            object.splice(index, 1);    // почистим массив из улетехших звёзд
-        }
-    });
-}
-
-function arrRemove(arr1, arr2, arr3) {
-    arr1.forEach(function (element1, index1, object1) {
-        arr2.forEach(function (element2, index2, object2) {
-            if (((element1.y - element2.y) <= (step - 50)) && (Math.abs(element1.x - element2.x) <= (step / 2))) {
-                arr3.push({ // пушим взрыв в 3ий массив
-                    x: element2.x,
-                    y: element2.y,
-                    ax: 0,
-                    ay: 0,
-                });
-                object1.splice(index1, 1);
-                object2.splice(index2, 1);    // почистим массив из улетехших пуль и астероид
-                player.score = player.score + 1; // увеличиваем счёт
-            };
-        });
-    });
-    return player.score;
-};
-
-function arrExpl(arr) {
-    arr.forEach(function (element, index, object) {
-            if (((player.y - element.y + 50) <= (imgH/2+step/2)) && (Math.abs(element.x - player.x+100) <= (imgW/2+step/2))) {
-                object.splice(index, 1);
-                player.lives = player.lives - 1; // уменьшаем жизни
-                vibrate(1000);
-            };
-        });
-    return player.lives;
-};
 
 // все обновления будут происходить тут
 
@@ -263,10 +237,54 @@ function update() {
             element.ax = 0;
         }
         if (element.ay > 9) {
-        object.splice(index, 1);
-    }
+            object.splice(index, 1);
+        }
     });
 
+    // заставляем звёзды двигаться по игрику вниз и удаляем из массива улетевшие
+    // будем использовать эту же функцию и для ракет
+    function arrMove(arr) {
+        arr.forEach(function (element, index, object) {
+            element.y = element.y + element.s;
+            if (element.y >= h || element.y <= 0) {
+                object.splice(index, 1);    // почистим массив из улетехших звёзд
+            }
+        });
+    }
+
+
+
+
+    function arrExpl(arr) {
+        arr.forEach(function (element, index, object) {
+            if (((player.y - element.y + 50) <= (imgH / 2 + step / 2)) && (Math.abs(element.x - player.x + 100) <= (imgW / 2 + step / 2))) {
+                object.splice(index, 1);
+                player.lives = player.lives - 1; // уменьшаем жизни
+                vibrate(500);
+            };
+        });
+        return player.lives;
+    };
+
+    function arrRemove(arr1, arr2, arr3) {
+        arr1.forEach(function (element1, index1, object1) {
+            arr2.forEach(function (element2, index2, object2) {
+                if (((element1.y - element2.y) <= (step - 50)) && (Math.abs(element1.x - element2.x) <= (step / 2))) {
+                    arr3.push({ // пушим взрыв в 3ий массив
+                        x: element2.x,
+                        y: element2.y,
+                        ax: 0,
+                        ay: 0,
+                    });
+                    object1.splice(index1, 1);
+                    object2.splice(index2, 1);    // почистим массив из улетехших пуль и астероид
+                    player.score = player.score + 1; // увеличиваем счёт
+                    clickSound();
+                };
+            });
+        });
+        return player.score;
+    };
 
     // вводим таймер, который срабатывает через определённое обновление фреймов и добавляем новые звёзды с верхней части экрана
     timer++;
@@ -281,6 +299,25 @@ function update() {
         });
     }
 
+    // вибрация при столкновении
+    function vibrate(val) {
+        if ("vibrate" in navigator) return navigator.vibrate(val);
+        if ("oVibrate" in navigator) return navigator.oVibrate(val);
+        if ("mozVibrate" in navigator) return navigator.mozVibrate(val);
+        if ("webkitVibrate" in navigator) return navigator.webkitVibrate(val);
+    }
+    function infiniteVibrate(val, interval) {
+        stopVibrate();
+        vInterval = setInterval(function () {
+            vibrate(val);
+        }, interval);
+    }
+    function stopVibrate() {
+        if (typeof (vInterval) !== "undefined") clearInterval(vInterval);
+        vibrate(0);
+    }
+
+
     if (timer % 30 == 0) {
         asteroids.push({
             x: random(0, w),
@@ -293,18 +330,11 @@ function update() {
     }
 }
 
-function oop() {
+function gameLoop() {
+    clickSoundInit(); // важно "запустить" звук по событию, т.е. нажатию кнопки
     render();
     update();
-    RAF(oop);
+    RAF(gameLoop);
 }
 
-
-oop();
-
-
-// подстраиваемся под ресайз окна
-window.addEventListener("resize", () => {
-    w = canvas.width = window.innerWidth;
-    h = canvas.height = window.innerHeight;
-})
+gameLoop();
